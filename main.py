8ed1a91acb8a7234a3dcb9e91a93531bf6a5b062
Vinícius Gabriel <timelord1025@gmail.com>
2021-09-04 12:43:37 -0300

    added average

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+from average import average
 from frequency import frequency_distribuition
 
 operations = {
-    '1': frequency_distribuition
+    '1': frequency_distribuition,
+    '2': average
 }
 
 while True:
     choice = input('''
     Operação:
     1 - Distribuição de frequências;
+    2 - Média;
     ''')
     op = operations[choice]
     op()
