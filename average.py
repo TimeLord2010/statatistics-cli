7913b2bbd5b77844430766765087aebefa6af837
Vinícius Gabriel <timelord1025@gmail.com>
2021-09-04 12:42:24 -0300

    added average

---
+++ average.py
@@ -0,0 +1,16 @@
+from utils import get_data
+import math
+
+def average ():
+    is_regular = input('''
+    1 - Regular;
+    2 - Ponderada.''')
+    if is_regular == '1':
+        data = get_data()
+        data = [float(d) for d in data]
+        return sum(data) / len(data)
+    else:
+        print('formato: valor*peso')
+        data = get_data()
+        data = [x.split('*') for x in data]
+        return sum([float(val)*float(weight) for (val, weight) in data]) / sum([x for (_, x) in data])
