713c625daf79c5e43868c117ed62e33592b6f65b
Vinícius Gabriel <timelord1025@gmail.com>
2021-09-04 14:43:33 -0300

    returns value and accepts data and is_regular as parameters

--- average.py
+++ average.py
@@ -1,17 +1,21 @@
 from utils import get_data
 import math
 
-def average ():
-    is_regular = input('''
-    1 - Regular;
-    2 - Ponderada.
-    ''')
+def average (data = None, is_regular = None):
+    if is_regular is None:
+        is_regular = input('''
+        1 - Regular;
+        2 - Ponderada.
+        ''')
     if is_regular == '1':
-        data = get_data()
+        if data is None:
+            data = get_data()
         data = [float(d) for d in data]
-        print(sum(data) / len(data))
+        avg = sum(data) / len(data)
     else:
-        print('formato: valor*peso')
-        data = get_data()
+        if data is None:
+            print('formato: valor*peso')
+            data = get_data()
         data = [x.split('*') for x in data]
-        print(sum([float(val)*float(weight) for (val, weight) in data]) / sum([x for (_, x) in data]))
+        avg = sum([float(val)*float(weight) for (val, weight) in data]) / sum([x for (_, x) in data])
+    return avg
