813e82f761289f9950ca376ea2749485e278b7c6
Vinícius Gabriel <timelord1025@gmail.com>
2021-09-04 13:15:47 -0300

    added meian and mode

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from average import average
 from frequency import frequency_distribuition
+from median import median
+from mode import mode
 
 operations = {
     '1': frequency_distribuition,
-    '2': average
+    '2': average,
+    '3': median,
+    '4': mode
 }
 
 while True:
@@ -11,6 +15,8 @@ while True:
     Operação:
     1 - Distribuição de frequências;
     2 - Média;
+    3 - Mediana;
+    4 - Moda;
     ''')
     op = operations[choice]
     op()
