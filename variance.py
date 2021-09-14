0164c8b04bca18ab2bc8607b84a2604c8fd5b3e5
Vinícius Gabriel <timelord1025@gmail.com>
2021-09-13 22:24:26 -0300

    now accepts avg as parameter

--- variance.py
+++ variance.py
@@ -2,12 +2,12 @@ from average import average
 from utils import get_data
 from math import pow
 
-def variance (data = None, ponderation = lambda i: 1, is_populational = False):
+def variance (data = None, ponderation = lambda i: 1, avg = None, is_populational = False):
     if data is None:
         data = get_data()
     data = [float(x) for x in data]
-    avg = average(data, is_regular='1')
-    n = len(data)
+    if avg is None:
+        avg = average(data, is_regular='1')
     if is_populational is None:
         is_populational = input('''
         1 - Amostral;
@@ -15,7 +15,13 @@ def variance (data = None, ponderation = lambda i: 1, is_populational = False):
         ''')
     else:
         is_populational = '2' if is_populational else '1'
+    total = 0
+    n = 0
+    for i, x in enumerate(data):
+        p = ponderation(i)
+        n += p
+        y = pow(x - avg, 2) * p
+        total += y
     if is_populational != '1':
         n -= 1
-    total = sum([pow(x - avg, 2)*ponderation(i) for i, x in enumerate(data)]) 
     return  total / n
