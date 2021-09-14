24e06979623f1cc0263ddf66e5f490b6e7e0887f
Vinícius Gabriel <timelord1025@gmail.com>
2021-09-13 21:43:08 -0300

    added class functions

---
+++ class_functions.py
@@ -0,0 +1,39 @@
+from variance import variance
+from utils import get_data
+
+def class_data ():
+    print('formato: <menor>-<maior>|<quantidade> ')
+    data = get_data()
+    t = []
+    for d in data:
+        [interval, quantity] = [x.strip() for x in d.split('|')]
+        [lower, upper] = [x.strip() for x in interval.split('-')]
+        point = float(lower) + ((float(upper) - float(lower)) / 2)
+        t.append((point, float(quantity)))
+    return t
+
+def class_average ():
+    sum_of_values = 0
+    pairs = class_data()
+    for (point, quantity) in pairs:
+        sum_of_values += point * quantity
+    n = sum([q for (_, q) in pairs])
+    return sum_of_values / n
+
+def class_variance ():
+    pairs = class_data()
+    data = [x for (x, _) in pairs]
+    return variance(data, lambda i: pairs[i][1])
+
+def class_functions ():
+    choice = input('''
+    1 - Média;
+    2 - Variância;
+    3 - Desvio padrão.
+    ''')
+    if choice == '1':
+        return class_average()
+    elif choice == '2':
+        return class_variance()
+    else:
+        pass
