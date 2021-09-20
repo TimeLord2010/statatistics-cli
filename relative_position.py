c0d3993a2ae44736dbe755f9185c6b3f57666874
Vinícius Gabriel <timelord1025@gmail.com>
2021-09-20 20:51:35 -0300

    added parameters

--- relative_position.py
+++ relative_position.py
@@ -1,10 +1,14 @@
-from utils import get_data
+from utils import get_float_data
 from average import average
 from standard_deviation import standard_deviation
 
-def relative_position ():
-    data = get_data()
+def relative_position (data = None, i = None, x = None):
+    if data is None:
+        data = get_float_data()
     avg = average(data)
     sd = standard_deviation(data)
-    i = int(input('Escolha o índice do item desejado.'))
-    return (data[i] - avg) / sd
+    if x is None:
+        if i is None:
+            i = int(input('Escolha o índice do item desejado.'))
+        x = data[i]
+    return (x - avg) / sd
